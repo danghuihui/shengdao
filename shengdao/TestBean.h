12414c0293d0f639187952cb16f2d26462347c54
huihui <1377655697@qq.com>
2019-06-26 23:41:35 +0800

    test

---
+++ shengdao/TestBean.h
@@ -0,0 +1,17 @@
+//
+//  TestBean.h
+//  shengdao
+//
+//  Created by apple on 2019/6/26.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestBean : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
